10b2edece5650d94c5484b7c229c613fe5e3a191
HugoCNRD <hugobatigne@gmail.com>
2026-03-17 18:30:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ def process_message(content):
     # Si le message est uniquement "6"
     if content.strip() == "6":
         return "7 !"
+
+def process_message(content):
+    # Si le message est uniquement "6"
+    if content.strip() == "six":
+        return "Seven !"
+
+def process_message(content):
+    # Si le message est uniquement "6"
+    if content.strip() == "Six":
+        return "Seven !"
     
     # Sinon, on barre tout sauf les "6"
     result = ""
@@ -23,6 +33,28 @@ def process_message(content):
     
     return f"{result}\n7 !"
 
+    result = ""
+    for char in content:
+        if char == "Six":
+            result += "Six"
+        elif char.strip() == "":
+            result += char  # garder les espaces
+        else:
+            result += f"~~{char}~~"
+    
+    return f"{result}\nSeven !"
+
+    result = ""
+    for char in content:
+        if char == "six":
+            result += "six"
+        elif char.strip() == "":
+            result += char  # garder les espaces
+        else:
+            result += f"~~{char}~~"
+    
+    return f"{result}\nSeven !"
+
 @client.event
 async def on_ready():
     print(f'Connecté en tant que {client.user}')
@@ -33,6 +65,14 @@ async def on_message(message):
         return
 
     if "6" in message.content:
+        response = process_message(message.content)
+        await message.reply(response)
+
+    if "Six" in message.content:
+        response = process_message(message.content)
+        await message.reply(response)
+
+    if "six" in message.content:
         response = process_message(message.content)
         await message.reply(response)
 
